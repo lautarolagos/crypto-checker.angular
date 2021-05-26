5986ec712a50ceaf0eb9ded385cd4109ec7490f3
FerminFernandez21 <fermin.f.b@hotmail.com>
2021-05-26 11:05:28 -0300

    cuarto commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
 import { TitleComponent } from './component/title/title.component';
-import { CalenderComponent } from './components/calender/calender.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    CalenderComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
